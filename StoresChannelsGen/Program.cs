f9174247fb9b3ad69488fae4d9bfd793155f7c6f
oathout@gmail.com <oathout@gmail.com>
2022-04-14 23:09:40 -0500

    clean

--- StoresChannelsGen/Program.cs
+++ StoresChannelsGen/Program.cs
@@ -1,20 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-List<string> topics = new List<string>();
-int chain = 2;
- 
 
-
-
-for (int i = 1; i < 10; i++)
+public static class Program
 {
-    string s = $"posretail_c{chain}_s{i}:1:1";
-    topics.Add(s);
-    for (int j = 1; j < 10; j++ )
+    public static void Main()
     {
-        s = $"posretail_c{chain}_s{i}_reg{j}:1:1";
-        topics.Add(s);
-    } 
+        List<string> topics = new List<string>();
+         
+
+        int[] stores = { 1, 2, 3 };
+        int[] terminals = { 4, 5, 6 };
+
+
+        string data = $"retail_sever_c{299}_s{9999}_txout:1:1";
+        string cmd = $"retail_sever_c{299}_s{9999}_cmd:1:1";
+        string info = $"retail_sever_c{299}_s{9999}_info:1:1";
+
+
+        topics.Add(data);
+        for (int j = 1; j < 10; j++)
+        {
+            s = $"posretail_c{299}_s{299}_reg{j}:1:1";
+            topics.Add(s);
+        }
+        Console.WriteLine(String.Join(",", topics));
+        Console.WriteLine("Hello, World!");
+
+        // Write your program code here. Make sure you call Console.ReadLine() at the end of the program to ensure that the console window pauses execution until a key press is received.
+        Console.WriteLine("Hello World!");
+        Console.ReadLine();
+    }
 }
-Console.WriteLine(String.Join(",",topics));
-Console.WriteLine("Hello, World!");
+
